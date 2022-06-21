03f369fac224b5a1e06d8a8f296fd96c263bab2b
Dovlat <dovlat93@gmail.com>
2022-06-21 19:31:30 +0200

    adds a sidebar component

--- components/AddProject.tsx
+++ components/AddProject.tsx
@@ -18,7 +18,12 @@ const AddProject = () => {
       });
 
   return isExpanded ? (
-    <form className="flex items-center justify-center gap-3" data-testid="add-project-inner">
+    <form
+      className="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-gray-900 hover:bg-gray-100 transition duration-300 ease-in-out"
+      data-mdb-ripple="true"
+      data-mdb-ripple-color="dark"
+      data-testid="add-project-inner"
+    >
       <input
         value={projectName}
         onChange={(e) => setProjectName(e.target.value)}
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,31 +3,33 @@ import useStore from 'store';
 import AddTodo from '@components/AddTodo';
 import Popup from '@components/Popup';
 import TodoList from '@components/TodoList';
-import AddProject from '@components/AddProject';
+import Sidebar from '@components/Sidebar';
 
 const Home: NextPage = () => {
   const { todos } = useStore((state) => state);
 
   return (
-    <div
-      style={{ minWidth: 250, maxWidth: 600, margin: 'auto' }}
-      className="w-full h-full flex flex-col justify-center items-center p-4"
-    >
-      <AddProject />
-      <AddTodo />
-      <TodoList />
-      <Popup>
-        <h2>Are you sure you want to delete the list?</h2>
-        <h1>THIS ACTION IS IRREVERSIBLE!</h1>
-      </Popup>
-      <button
-        type="button"
-        className="button"
-        disabled={todos.length === 0}
+    <main>
+      <Sidebar />
+      <div
+        style={{ minWidth: 250, maxWidth: 600, margin: 'auto' }}
+        className="w-full h-full flex flex-col justify-center items-center p-4"
       >
-        Clear list
-      </button>
-    </div>
+        <AddTodo />
+        <TodoList />
+        <Popup>
+          <h2>Are you sure you want to delete the list?</h2>
+          <h1>THIS ACTION IS IRREVERSIBLE!</h1>
+        </Popup>
+        <button
+          type="button"
+          className="button"
+          disabled={todos.length === 0}
+        >
+          Clear list
+        </button>
+      </div>
+    </main>
   );
 };
 
