fd2e278285f509aefad368ffc0fdd14c7dbc9996
Dovlat <dovlat93@gmail.com>
2022-06-21 19:53:50 +0200

    adds custom list style type

--- components/AddProject.tsx
+++ components/AddProject.tsx
@@ -17,60 +17,48 @@ const AddProject = () => {
         setIsExpanded(false);
       });
 
-  return isExpanded ? (
-    <form
-      className="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-gray-900 hover:bg-gray-100 transition duration-300 ease-in-out"
-      data-mdb-ripple="true"
-      data-mdb-ripple-color="dark"
-      data-testid="add-project-inner"
+  return (
+    <details
+      className="flex flex-col items-center text-sm py-4 px-6 h-auto overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-gray-900 hover:bg-gray-100 transition-all duration-300 ease-in-out"
     >
-      <input
-        value={projectName}
-        onChange={(e) => setProjectName(e.target.value)}
-        className=":placeholder-gray-500 p-1.5 px-4 outline-none border-2 border-solid border-gray-400 rounded"
-        data-testid="project-name"
-        type="text"
-        placeholder="Name your project"
-      />
-      <button
-        className="btn-black"
-        type="button"
-        onClick={() => addProject()}
-        data-testid="add-project-submit"
+      <summary className="list-plus">Add Project</summary>
+      <form
+        className="flex flex-col items-center text-sm p-6 h-auto overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-gray-900 hover:bg-gray-100 transition duration-300 ease-in-out"
+        data-mdb-ripple="true"
+        data-mdb-ripple-color="dark"
+        data-testid="add-project-inner"
       >
-        Add Project
-      </button>
-      <span
-        aria-label="Cancel adding project"
-        data-testid="hide-project-overlay"
-        className="btn-black-outline"
-        onClick={() => setIsExpanded(false)}
-        onKeyDown={(e) => {
-          if (e.key === 'Enter') setIsExpanded(false);
-        }}
-        role="button"
-        tabIndex={0}
-      >
-        Cancel
-      </span>
-    </form>
-  ) : (
-    <section className="text-red-700 display-flex" data-testid="add-project">
-      <span className="text-red-700 text-lg">&#43;</span>
-      <span
-        aria-label="Add Project"
-        data-testid="add-project-action"
-        className="text-base"
-        onClick={() => setIsExpanded((prev) => !prev)}
-        onKeyDown={(e) => {
-          if (e.key === 'Enter') setIsExpanded((prev) => !prev);
-        }}
-        role="button"
-        tabIndex={0}
-      >
-        Add Project
-      </span>
-    </section>
+        <input
+          value={projectName}
+          onChange={(e) => setProjectName(e.target.value)}
+          className=":placeholder-gray-500 p-1.5 px-4 outline-none border-2 border-solid border-gray-400 rounded"
+          data-testid="project-name"
+          type="text"
+          placeholder="Name your project"
+        />
+        <button
+          className="btn-black"
+          type="button"
+          onClick={() => addProject()}
+          data-testid="add-project-submit"
+        >
+          Add Project
+        </button>
+        <span
+          aria-label="Cancel adding project"
+          data-testid="hide-project-overlay"
+          className="btn-black-outline"
+          onClick={() => setIsExpanded(false)}
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') setIsExpanded(false);
+          }}
+          role="button"
+          tabIndex={0}
+        >
+          Cancel
+        </span>
+      </form>
+    </details>
   );
 };
 
