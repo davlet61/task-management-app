a96511469c29d971b357c1c303ceb24604a285e3
Dovlat <dovlat93@gmail.com>
2022-06-25 16:49:27 +0200

    adds a spinner and loading page

---
+++ src/components/Loading.tsx
@@ -0,0 +1,8 @@
+import { Spinner } from './SVGs';
+
+const Loading = () => (
+  <main className="flex items-center justify-center h-[100vh] bg-neutral-100">
+    <Spinner />
+  </main>
+);
+export default Loading;
